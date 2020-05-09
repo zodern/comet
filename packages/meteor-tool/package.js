9d3276a9376f7641e423fd55f491f9c031ca404c
zodern <zodern@users.noreply.github.com>
2020-05-08 22:15:21 -0500

    Add release config

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'zodern:meteor-tool',
   summary: "The Meteor command-line tool",
-  version: '1.10.2-comet.1'
+  version: '1.10.2-comet.2'
 });
 
 Package.includeTool();
